c8430452f99b5c7045d2aca3bdf28f170835c349
Brendon Richards <brendonrichards93@gmail.com>
2018-08-15 11:32:44 +0000

    Create Navigation & Perform a Few Touchups

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 // Load the things we need
 const express = require("express"),
+      path = require("path"),
       app = express();
       
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Set view engine to ejs
 app.set("view engine", "ejs");
 
 // Render Home page
 app.get("/", (req, res) => {
   res.render("pages/index", {
-    pageTitle: "Home Page"
+    pageTitle: "All Notebooks"
   });
 });
 
