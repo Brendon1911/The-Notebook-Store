6ecbd20f1a05ca9d31d1ce84310e1bb2d27a225a
Brendon Richards <brendonrichards93@gmail.com>
2018-08-15 13:15:09 +0000

    Create temporary notebook data & display in index.ejs

--- app.js
+++ app.js
@@ -10,8 +10,31 @@ app.set("view engine", "ejs");
 
 // Render Home page
 app.get("/", (req, res) => {
+  
+  let notebooks = [
+      {
+        make: "Dell"   ,
+        model: "Insperion",
+        image: "https://i5.walmartimages.com/asr/30479c85-5ec5-41be-a289-83d235cf0207_1.b94e70cc3d493cac7380cb0ef5aa636e.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF",
+        price: "$100.95"
+      },
+      {
+        make: "Acer"   ,
+        model: "Aspire",
+        image: "https://assets.pcmag.com/media/images/352181-acer-aspire-e1-510p-2671.jpg?width=1000&height=804",
+        price: "$100.95"
+      },
+      {
+        make: "Mecer"   ,
+        model: "Xpression",
+        image: "https://media.takealot.com/covers_tsins/51572818/Z140CPLUS-1-zoom.jpg",
+        price: "$100.95"
+      }
+  ];
+  
   res.render("pages/index", {
-    pageTitle: "All Notebooks"
+    pageTitle: "All Notebooks",
+    notebooks: notebooks
   });
 });
 
